00f235ac30055b7836e17a7267fae24f45db714c
Adonais0 <chuyao@umich.edu>
2019-03-11 00:36:23 -0400

    support special characters

--- ffx_helper.py
+++ ffx_helper.py
@@ -1,7 +1,7 @@
 import pyffx
 
 import string, logging, sys
-
+import re
 from autologging import logged, traced
 
 logger = logging.getLogger()
@@ -43,44 +43,28 @@ class FFXEncrypt():
                 enc = e.encrypt(val)
             else: # Either String or Decimal
                 val = str(val)
-                vlen = len(val)
-                try: # If val is decimal 
-                    fl = float(val) # Test if val is a valid decimal, otherwise val is not a numeric value
-                    if 'E' in val or 'e' in val: # If val is in scientific notation
-                        val = str(fl)
-
-                    neg = False
-                    enc = ""
-                    
-                    if val.startswith('-'):
-                        val = val[1:]
-                        neg = True
-                    
-                    if '.' in val:
-                        first, second = val.split('.')
-                        first_encrypt = ""
-                        second_encrypt = ""
-                        
-                        if first != "" and first.isdigit():
-                            e_1 = pyffx.Integer(self.ffx_secret, length=len(first))
-                            first_encrypt = e_1.encrypt(first)
-                        if second != "" and second.isdigit():
-                            e_2 = pyffx.Integer(self.ffx_secret, length=len(second))
-                            second_encrypt = e_2.encrypt(second)
-                        enc = '.'.join([str(first_encrypt), str(second_encrypt)])
-                    
-                    if neg:
-                        enc = '-' + str(enc)
-
-                except ValueError: # If val is String, the first float cast will throw a ValueError
-                    # Test if lowercase or uppercase or mixed
-                    if val.islower():
-                        e = pyffx.String(self.ffx_secret, alphabet=string.ascii_lowercase, length=vlen)
-                    elif val.isupper():
-                        e = pyffx.String(self.ffx_secret, alphabet=string.ascii_uppercase, length=vlen)
-                    else:
-                        e = pyffx.String(self.ffx_secret, alphabet=string.ascii_letters, length=vlen)
-                    enc = e.encrypt(val)
+                enc = ""
+                elems = re.split('(\W+|\d+)', val)
+                for elem in elems:
+                    if len(elem) == 0:
+                        continue
+                    vlen = len(elem)
+                    if elem.isdigit():
+                        # encrypt integer part
+                        e = pyffx.Integer(self.ffx_secret, length=vlen)
+                        temp = str(e.encrypt(elem))
+                    elif elem.isalpha():
+                        # a-zA-Z
+                        if elem.islower():
+                            e = pyffx.String(self.ffx_secret, alphabet=string.ascii_lowercase, length=vlen)
+                        elif elem.isupper():
+                            e = pyffx.String(self.ffx_secret, alphabet=string.ascii_uppercase, length=vlen)
+                        else:
+                            e = pyffx.String(self.ffx_secret, alphabet=string.ascii_letters, length=vlen)
+                        temp = e.encrypt(elem)
+                    else: # special characters
+                        temp = elem
+                    enc += str(temp)
 
             logger.debug(f"Out val {enc}")  
             # Return it as a string 
