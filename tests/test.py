66e0d6c04850d02d1f04f08920ea5a7771c8ef18
Chuyao Wang <31697300+Adonais0@users.noreply.github.com>
2019-03-25 16:55:55 -0400

    Issue 30 (#31)
    * Increase faker and ffx encrypt key length to >=16

--- tests/test.py
+++ tests/test.py
@@ -20,55 +20,55 @@ logging.basicConfig(level=os.getenv("log_level", "TRACE"))
 class TestAnonymizer(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.ffx = FFXEncrypt("testpass")
+        cls.ffx = FFXEncrypt("passwordpassword")
         cls.faker = Faker()
         cls.faker.add_provider(CustomProvider)
 
     def test_ffx_encrypt(self):
-        self.assertEqual(self.ffx.encrypt("ABC"), 'FCG')
+        self.assertEqual(self.ffx.encrypt("ABC"), 'HZC')
     def test_ffx_prefix(self):
         self.assertEqual(self.ffx.encrypt(123456, addition=123000), 123000+self.ffx.encrypt(456))
         # Test if no prefix found
-        self.assertEqual(self.ffx.encrypt(995), 68)
+        self.assertEqual(self.ffx.encrypt(995), 643)
     def test_ffx_font_case(self):
-        self.assertEqual(self.ffx.encrypt("something"), "vdnovqovo")
-        self.assertEqual(self.ffx.encrypt("someTHing"), "cLxUGFKus")
-        self.assertEqual(self.ffx.encrypt("SOMETHING"), "VDNOVQOVO")
+        self.assertEqual(self.ffx.encrypt("something"), "zkcofcwmq")
+        self.assertEqual(self.ffx.encrypt("someTHing"), "KHbVXLxYV")
+        self.assertEqual(self.ffx.encrypt("SOMETHING"), "ZKCOFCWMQ")
     def test_ffx_decimal(self):
-        self.assertEqual(self.ffx.encrypt('10.123'), '73.453')
-        self.assertEqual(self.ffx.encrypt('0.123'), '2.453')
-        self.assertEqual(self.ffx.encrypt('.567'), '.909')
-        self.assertEqual(self.ffx.encrypt('-100.0'), '-79.2')
-        self.assertEqual(self.ffx.encrypt('23.55'), '82.34')
-        self.assertEqual(self.ffx.encrypt('-.567'), '-.909')
-        self.assertEqual(self.ffx.encrypt('-12.567'), '-14.909')
-        self.assertEqual(self.ffx.encrypt('6.000'), '8.779')
-        self.assertEqual(self.ffx.encrypt('1.23E-2'), '3.82Q-4')
-        self.assertEqual(self.ffx.encrypt('-1.23e2'), '-3.82q4')
+        self.assertEqual(self.ffx.encrypt('10.123'), '80.680')
+        self.assertEqual(self.ffx.encrypt('0.123'), '4.680')
+        self.assertEqual(self.ffx.encrypt('.567'), '.80')
+        self.assertEqual(self.ffx.encrypt('-100.0'), '-149.4')
+        self.assertEqual(self.ffx.encrypt('23.55'), '58.68')
+        self.assertEqual(self.ffx.encrypt('-.567'), '-.80')
+        self.assertEqual(self.ffx.encrypt('-12.567'), '-25.80')
+        self.assertEqual(self.ffx.encrypt('6.000'), '0.923')
+        self.assertEqual(self.ffx.encrypt('1.23E-2'), '5.58C-6')
+        self.assertEqual(self.ffx.encrypt('-1.23e2'), '-5.58c6')
     def test_ffx_special(self):
-        self.assertEqual(self.ffx.encrypt('test@example.com'), 'fjjs@qdhieex.lgs')
-        self.assertEqual(self.ffx.encrypt('test@@@@gmail.colll099m'), 'fjjs@@@@aibdq.whecu236y')
-        self.assertEqual(self.ffx.encrypt('123abc.456.bda123'), '453fcg.982.cle453')
+        self.assertEqual(self.ffx.encrypt('test@example.com'), 'cafw@uegmpnf.vxj')
+        self.assertEqual(self.ffx.encrypt('test@@@@gmail.colll099m'), 'cafw@@@@nyzav.ddzvu628k')
+        self.assertEqual(self.ffx.encrypt('123abc.456.bda123'), '680hzc.605.qfu680')
     def test_assignment_custom(self):
-        self.faker.seed(util_methods.hashStringToInt("testpass", 8))
+        self.faker.seed(util_methods.hashStringToInt("testpasstestpass", 16))
         # pylint: disable=no-member
-        self.assertEqual(self.faker.assignment(),"Information Assignment #531")
+        self.assertEqual(self.faker.assignment(),"Practice Assignment #196")
         # pylint: disable=no-member
-        self.assertEqual(self.faker.assignment(),"Architecture Assignment #916")
+        self.assertEqual(self.faker.assignment(), "Architecture Assignment #819")
 
     def test_date_time_on_date(self):
-        self.faker.seed(util_methods.hashStringToInt("testpass", 8))
+        self.faker.seed(util_methods.hashStringToInt("testpasstestpass", 16))
         # pylint: disable=no-member
-        self.assertEqual(self.faker.date_time_on_date(datetime.datetime(2019, 1, 1, 1, 1, 1)), datetime.datetime(2019, 1, 1, 18, 53, 31))
+        self.assertEqual(self.faker.date_time_on_date(datetime.datetime(2019, 1, 1, 1, 1, 1)), datetime.datetime(2019, 1, 1, 6, 58, 12))
 
         # Adding the string case for date_time_on_date
         # pylint: disable=no-member
-        self.assertEqual(self.faker.date_time_on_date("2019-05-01 13:14:15"), datetime.datetime(2019, 5, 1, 12, 31, 18))
+        self.assertEqual(self.faker.date_time_on_date("2019-05-01 13:14:15"), datetime.datetime(2019, 5, 1, 11, 58, 39))
         
     def test_course_id(self):
-        self.faker.seed(util_methods.hashStringToInt("testpass", 8))
-        self.assertEqual(self.faker.course(),"AUTO 631 006 SP 2064")
-        self.assertEqual(self.faker.course(), "LATIN 270 003 SP 2024")
+        self.faker.seed(util_methods.hashStringToInt("testpasstestpass", 16))
+        self.assertEqual(self.faker.course(),"AUTO 296 006 FA 2073")
+        self.assertEqual(self.faker.course(), "AUTO 273 007 SP 2026")
 
 if __name__ == '__main__':
     unittest.main()
